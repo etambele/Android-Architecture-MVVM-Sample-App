906071a0dc044dd85cb4618ebb94c79af3aad2f2
Emmanuel <etambele@gmail.com>
2019-07-23 16:17:12 -0400

    repository created

--- androidMVVMdemo/app/src/main/java/com/ambell/androidmvvmdemo/NoteDatabase.java
+++ androidMVVMdemo/app/src/main/java/com/ambell/androidmvvmdemo/NoteDatabase.java
@@ -1,10 +1,13 @@
 package com.ambell.androidmvvmdemo;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = Note.class, version = 1) //{} more than one entity use array {Note.class, Note2.class}
 public abstract class NoteDatabase extends RoomDatabase {
@@ -18,8 +21,34 @@ public abstract class NoteDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void,Void,Void>{
+
+        private NoteDao noteDao;
+
+        private PopulateDbAsyncTask(NoteDatabase db){
+            noteDao = db.noteDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            noteDao.insert(new Note("Title 1", "description 1", 1));
+            noteDao.insert(new Note("Title 2", "description 2", 2));
+            noteDao.insert(new Note("Title 3", "description 3", 3));
+            return null;
+        }
     }
 }
