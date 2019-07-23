0df1d7dc062176c04bb470b40a9c6fb4b7a723da
Emmanuel <etambele@gmail.com>
2019-07-23 17:48:27 -0400

    View Model

--- androidMVVMdemo/app/src/main/java/com/ambell/androidmvvmdemo/MainActivity.java
+++ androidMVVMdemo/app/src/main/java/com/ambell/androidmvvmdemo/MainActivity.java
@@ -1,13 +1,29 @@
 package com.ambell.androidmvvmdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, notes -> {
+            //update RecyclerView
+            Toast.makeText(MainActivity.this, "unchanged", Toast.LENGTH_SHORT).show();
+        });
     }
 }
